c3ed5fd7324632de34cf7625c0ea43bbc402aac8
deathbreaker <oristgrey@gmail.com>
2018-05-25 01:46:26 +0200

    Patch

--- backend/resources/views/index.blade.php
+++ backend/resources/views/index.blade.php
@@ -6,12 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="{{mix('css/app.css')}}">
+    <link rel="stylesheet" href="/css/app.css">
     <title>Zoo App</title>
 </head>
 <body>
 <div id="app"></div>
 
-<script src="{{ mix('js/app.js') }}"></script>
+<script src="/js/app.js"></script>
 </body>
 </html>
