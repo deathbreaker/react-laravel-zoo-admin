2294453d31ff8658cecdb4ac6a5113cc6db1544f
deathbreaker <oristgrey@gmail.com>
2018-05-25 09:30:14 +0200

    Commit

--- frontend/UI/react/app/utils/ajax.js
+++ frontend/UI/react/app/utils/ajax.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const ajax = axios.create({
-    baseURL: process.env.NODE_ENV === "production" ? ": http://209.97.140.65/api" : "http://localhost:8000/api",
+    baseURL: process.env.NODE_ENV === "production" ? "http://209.97.140/api" : "http://localhost:8000/api",
     withCredentials: true,
 });
 
