c3fe8a0ce273b80772c404464ad9fef4707fc37a
deathbreaker <oristgrey@gmail.com>
2018-05-25 05:42:46 +0200

    Last commit

--- backend/resources/views/index.blade.php
+++ backend/resources/views/index.blade.php
@@ -4,14 +4,14 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Zoo App</title>
 </head>
 <body>
 <div id="app"></div>
 
-<script src="/js/app.js"></script>
+<script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
