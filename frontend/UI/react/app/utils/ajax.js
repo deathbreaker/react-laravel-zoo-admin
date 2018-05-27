ffd433b1fc758766350d9ac8488b8287f3b8de36
deathbreaker <oristgrey@gmail.com>
2018-05-27 21:38:41 +0200

    With app.js, app.css

--- frontend/UI/react/app/utils/ajax.js
+++ frontend/UI/react/app/utils/ajax.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const ajax = axios.create({
-    baseURL: process.env.NODE_ENV === "production" ? "http://159.65.87.193/api" : "http://localhost:8000/api",
+    baseURL: process.env.NODE_ENV === "production" ? "http://209.97.140.65/api" : "http://localhost:8000/api",
     withCredentials: true,
 });
 
